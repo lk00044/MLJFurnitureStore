1bdf562c253692ba81523af25c38ceb810a6912a
Lauramarie Laskey <ll00098@my.westga.edu>
2024-03-17 21:14:01 -0400

    Created loginform w/ user and password fields

---
+++ cs6232-g4/View/LoginForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace cs6232_g4.View
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            TableLayoutPanel = new TableLayoutPanel();
+            LoginBox = new GroupBox();
+            LblUsername = new Label();
+            LblPassword = new Label();
+            TextPassword = new TextBox();
+            TextUsername = new TextBox();
+            TableLayoutPanel.SuspendLayout();
+            LoginBox.SuspendLayout();
+            SuspendLayout();
+            // 
+            // TableLayoutPanel
+            // 
+            TableLayoutPanel.ColumnCount = 1;
+            TableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            TableLayoutPanel.Controls.Add(LoginBox, 0, 0);
+            TableLayoutPanel.Location = new Point(0, 0);
+            TableLayoutPanel.Name = "TableLayoutPanel";
+            TableLayoutPanel.RowCount = 2;
+            TableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            TableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            TableLayoutPanel.Size = new Size(621, 325);
+            TableLayoutPanel.TabIndex = 1;
+            // 
+            // LoginBox
+            // 
+            LoginBox.Controls.Add(TextUsername);
+            LoginBox.Controls.Add(TextPassword);
+            LoginBox.Controls.Add(LblPassword);
+            LoginBox.Controls.Add(LblUsername);
+            LoginBox.Location = new Point(3, 3);
+            LoginBox.Name = "LoginBox";
+            LoginBox.Size = new Size(615, 151);
+            LoginBox.TabIndex = 0;
+            LoginBox.TabStop = false;
+            LoginBox.Text = "Login";
+            // 
+            // LblUsername
+            // 
+            LblUsername.AutoSize = true;
+            LblUsername.Font = new Font("Segoe UI", 12F);
+            LblUsername.Location = new Point(33, 45);
+            LblUsername.Name = "LblUsername";
+            LblUsername.Size = new Size(99, 28);
+            LblUsername.TabIndex = 0;
+            LblUsername.Text = "Username";
+            LblUsername.Click += label1_Click_1;
+            // 
+            // LblPassword
+            // 
+            LblPassword.AutoSize = true;
+            LblPassword.Font = new Font("Segoe UI", 12F);
+            LblPassword.Location = new Point(33, 100);
+            LblPassword.Name = "LblPassword";
+            LblPassword.Size = new Size(93, 28);
+            LblPassword.TabIndex = 1;
+            LblPassword.Text = "Password";
+            // 
+            // TextPassword
+            // 
+            TextPassword.Location = new Point(182, 100);
+            TextPassword.Name = "TextPassword";
+            TextPassword.Size = new Size(390, 27);
+            TextPassword.TabIndex = 2;
+            // 
+            // TextUsername
+            // 
+            TextUsername.Location = new Point(182, 45);
+            TextUsername.Name = "TextUsername";
+            TextUsername.Size = new Size(390, 27);
+            TextUsername.TabIndex = 3;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(624, 327);
+            Controls.Add(TableLayoutPanel);
+            Name = "LoginForm";
+            Text = "Login";
+            Load += LoginForm_Load;
+            TableLayoutPanel.ResumeLayout(false);
+            LoginBox.ResumeLayout(false);
+            LoginBox.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel TableLayoutPanel;
+        private GroupBox LoginBox;
+        private Label LblPassword;
+        private Label LblUsername;
+        private TextBox TextUsername;
+        private TextBox TextPassword;
+    }
+}
---
+++ cs6232-g4/View/LoginForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cs6232_g4.View
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginBox_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
